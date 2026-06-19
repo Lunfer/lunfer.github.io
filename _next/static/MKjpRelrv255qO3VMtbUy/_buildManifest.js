be92b77a27c0745991ae0857c0f51d2d25ce1910
Lunfer <68249454+Lunfer@users.noreply.github.com>
2026-06-19 16:14:59 +0000

    Deploying to gh-pages from @ Lunfer/lunfer.github.io@ed8dacba3dc1cfb4149eaf5668c14769797a8b0c 🚀

--- _next/static/MKjpRelrv255qO3VMtbUy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/704-e2d2630cdb4702cb.js","static/css/3716d630742123bf.css","static/chunks/pages/index-52fe42e7ddeb059a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
